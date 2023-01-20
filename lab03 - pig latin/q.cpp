a30990573d1d9314969a1c8e923c5b67caf79e15
benjamin <benjaminzhiyuan.lee@edu.digipen>
2023-01-20 14:51:28 +0800

    pig

--- lab03 - pig latin/q.cpp
+++ lab03 - pig latin/q.cpp
@@ -1,2 +1,58 @@
+#include <iostream> // std::cout
+#include <iomanip>  // std::setw, std::endl 
+#include <string>   // std::string
+#include <fstream>  // std::ifstream
+#include <vector>   // std::vector<std::string>
+#include <iterator> // std::istream_iterator<std::string>
+#include "q.hpp"    // to_piglatin
 // See the specs for more information on how to author q.cpp ...
 // don't forget - all names introduced by you must be in namespace hlp2 ...
+namespace hlp2
+{
+    std::string to_piglatin(std::string word)
+    {
+        std::string yay = "-yay";
+        std::string ay = "-ay";
+        std::string way = "-way";
+        std::string temp;
+        std::string firstcharacter;
+        firstcharacter=word.at(0);
+
+        if(word.find_first_of("aeiouAEIOU")==std::string::npos)
+        {
+        word.append(way);
+        return word;
+        }
+        int i=0;
+        if(firstcharacter.find_first_of("aeiouAEIOU")==std::string::npos)
+        {   
+            if(firstcharacter.find_first_of("yY")!=std::string::npos)
+            {
+            i = word.find_first_of("aeiouAEIOU");
+            temp = word.substr(0,i);
+            word.erase(0,i);
+            word.append(temp);
+            word.append(ay);
+            return word;
+            }
+            else
+            {
+            i = word.find_first_of("aeiouAEIOUyY");
+            temp = word.substr(0,i);
+            word.erase(0,i);
+            word.append(temp);
+            word.append(ay);
+            return word;               
+            }
+        }
+
+        if(firstcharacter.find_first_of("aeiouAEIOU")!=std::string::npos)
+        {
+            word.append(yay);
+            return word;
+        }
+
+
+        return word;
+    }
+}
--- lab03 - pig latin/q.hpp
+++ lab03 - pig latin/q.hpp
@@ -1,2 +1,7 @@
 // See the specs for more information on how to author q.hpp ...
 // don't forget - all names introduced by you must be in namespace hlp2 ...
+
+namespace hlp2
+{
+    std::string to_piglatin(std::string word);
+}
