d466b682d4b58722c3209cac89b68a997dcc5eb8
benjamin <benjaminzhiyuan.lee@edu.digipen>
2023-02-03 14:48:53 +0800

    lab05

--- lab05/point.cpp
+++ lab05/point.cpp
@@ -1,3 +1,16 @@
+/*!*****************************************************************************
+\file    point.cpp
+\author  Benjamin Lee Zhi Yuan
+\par     DP email: benjaminzhiyuan.lee
+\par     Course: CSD1171
+\par     Section: A
+\par     Lab 05
+\date    03-02-23
+
+\brief
+
+**********************************************************************************/
+
 // Since your clients will not have access to this file, your initial
 // inclination will be to avoid documenting this file ...
 // However, since you're a client too and will need to understand this
@@ -8,25 +21,46 @@
 // function-level documentation for each function that you'll implement to
 // avoid unnecessary grade deductions.
 
-#include "point.hpp"  // Point interface
-#include <cmath>      // sin, cos, sqrt
+#include "point.hpp" // Point interface
+#include <cmath>     // sin, cos, sqrt
 
 ///////////////////////////////////////////////////////////////////////////////
 // declare private stuff here [if any] in this anonymous namespace ...
-namespace {
+namespace
+{
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace hlp2 {
+namespace hlp2
+{
 
-// define 3 constructors
-// define 8 member functions
-// define 15 non-member, non-friend functions
+    // define 3 constructors
+    Point::Point() : x{0.0}, y{0.0}
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+
+    Point::Point(int a, int b) : x{a}, y{b}
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+
+    Point::Point(double a, double b) : x{a}, y{b}
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+
+    // define 8 member functions
+    
+
+    // define 15 non-member, non-friend functions
+
 
 } // end hlp2 namespace
 
 ///////////////////////////////////////////////////////////////////////////////
 // define private stuff here [if any] in this anonymous namespace ...
-namespace {
+namespace
+{
 }
--- lab05/point.hpp
+++ lab05/point.hpp
@@ -1,3 +1,16 @@
+/*!*****************************************************************************
+\file    point.hpp
+\author  Benjamin Lee Zhi Yuan
+\par     DP email: benjaminzhiyuan.lee
+\par     Course: CSD1171
+\par     Section: A
+\par     Lab 05
+\date    03-02-23
+
+\brief
+
+**********************************************************************************/
+
 // make sure to provide file-level documentation and function-level
 // documentation for each function that you'll implement to avoid
 // unnecessary grade deductions.
@@ -10,12 +23,16 @@
 #include <iostream> // istream, ostream
 
 namespace hlp2 {
-	
 class Point {
 public:
 	// declare constructors (3)
+	Point(); //default
+	Point(int,int); //int int
+	Point(double,double); //double double
 
 	// declare overloaded operators (8 member functions)
+	Point Point::operator[](int index);
+	Point Point::operator+=()
 
 private:
 	double x; // The x-coordinate of a Point
