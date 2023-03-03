030430fae0d2ab47e5bac075bb3e8d5c752cea05
benjamin <benjaminzhiyuan.lee@digipen.edu>
2023-03-04 02:07:00 +0800

    completed lab08

--- lab08 - sll using classes/listint.hpp
+++ lab08 - sll using classes/listint.hpp
@@ -1,8 +1,21 @@
-// Provide include guards
-// Don't include either of the following two headers: <forward_list> and <list>
-// You may need to include <cstddef> for size_t [which is the largest unsigned type from the C standard library]
+/*!******************************************************************
+ * \file      listint.hpp
+ * \author    Benjamin Lee
+ * \par       DP email: benjaminzhiyuan.lee\@digipen.edu.sg
+ * \par       Course: CSD1171
+ * \par       Section: A
+ * \par       Lab 08
+ * \date      03-03-2023
+ *
+ * \brief
+ * This file defines and implements a class ListInt that encapsulates a singly-linked list of int values.
+ *********************************************************************/
+#ifndef LISTINT_H
+#define LISTINT_H
+
 #include <cstddef>
-#include <vector>
+#include <iostream>
+#include <initializer_list>
 // All names from here on must be in namespace hlp2.
 // Don't add any names that are private to the implementation source file listint.cpp in this file.
 // Necessary declarations are described in the spec.
@@ -15,226 +28,96 @@ namespace hlp2
     class ListInt
     {
     public:
-        // type aliases
-        using size_type = std::size_t;
         using value_type = int;
-        using reference = value_type &;
-        using const_reference = const value_type &;
-        using pointer = value_type *;
-        using const_pointer = const value_type *;
+        using size_type = std::size_t;
 
-    private:
-        struct Node
-        {
-            int data;   // the actual data in the node
-            Node *next; // pointer to the next Node
-        };
-        Node *head{nullptr};  // pointer to the head of the list
-        Node *tail{nullptr};  // pointer to the last node
-        size_type counter{0}; // number of nodes on the list
-        Node *new_node(value_type data) const;
+        // Default constructor
+        ListInt();
 
-    public:
-        // interface
-        friend std::ostream &operator<<(std::ostream &os, const ListInt &rhs);
+        // Copy constructor
+        ListInt(const ListInt &other);
 
-        static size_type object_count();
+        // Constructor that takes an initializer list
+        ListInt(std::initializer_list<value_type> ilist);
 
+        // Destructor
+        ~ListInt();
+
+        // Copy assignment operator
+        ListInt &operator=(ListInt other);
+
+        // Copy assignment operator that takes an initializer list
+        ListInt &operator=(std::initializer_list<value_type> ilist);
+
+        // Concatenate two ListInt objects using operator+
+        ListInt operator+(const ListInt &other) const;
+
+        // Concatenate a ListInt object and an initializer list using operator+
+        ListInt operator+(std::initializer_list<value_type> ilist) const;
+
+        // Concatenate an initializer list and a ListInt object using operator+
+        friend ListInt operator+(std::initializer_list<value_type> ilist, const ListInt &rhs);
+
+        // Concatenate another ListInt object to this list using the += operator
+        ListInt &operator+=(const ListInt &other);
+
+        // Concatenate an rvalue reference to another ListInt object to this list using the += operator
+        ListInt &operator+=(ListInt &&other);
+
+        // Overload of operator[] that returns a reference to the data of the node at index i
+        value_type &operator[](size_t i);
+
+        // Const overload of operator[] that returns a const reference to the data of the node at index i
+        const value_type &operator[](size_t i) const;
+
+        // Add a value to the front of the list
+        void push_front(value_type value);
+
+        // Add a value to the back of the list
+        void push_back(value_type value);
+
+        // Remove the front node and return its data
+        value_type pop_front();
+
+        // Erase all nodes in the list
+        void clear();
+
+        // Exchange the contents of the list with another list
+        void swap(ListInt &other);
+
+        // Return the number of nodes in the list
+        size_t size() const;
+
+        // Return true if the list is empty, false otherwise
         bool empty() const;
 
-        size_type size() const
-        {
-            size_type count = 0;
-            Node *current = head;
-            while (current != nullptr)
-            {
-                ++count;
-                current = current->next;
-            }
-            return count;
-        }
+        // Return the current value of object_counter
+        static size_type object_count();
 
-        // three ctors:
-        ListInt() : head(nullptr), tail(nullptr), counter(0)
-        {
-        }
-
-        ListInt(const ListInt &other) : ListInt()
-        {
-            *this += other;
-        }
-
-        ListInt(std::initializer_list<value_type> ilist) : ListInt()
-        {
-            for (auto &&element : ilist)
-            {
-                push_back(element);
-            }
-        }
-
-        // destructor
-        ~ListInt()
-        {
-            clear();
-        }
-
-        // copy assignment with another ListInt object
-        ListInt &operator=(ListInt other)
-        {
-            std::swap(head, other.head);
-            std::swap(tail, other.tail);
-            std::swap(counter, other.counter);
-            return *this;
-        }
-
-        // copy assignment with std::initializer_list<value_type>
-        ListInt &operator=(std::initializer_list<value_type> ilist)
-        {
-            ListInt temp(ilist);
-            *this = temp;
-            return *this;
-        }
-
-        // operator += overload: concatenates with nodes of another ListInt container
-        ListInt &operator+=(const ListInt &other)
-        {
-            for (auto &&element : other)
-            {
-                push_back(element);
-            }
-            return *this;
-        }
-
-        // operator += overload: concatenates with std::initializer_list<value_type>
-        ListInt &operator+=(std::initializer_list<value_type> ilist)
-        {
-            for (auto &&element : ilist)
-            {
-                push_back(element);
-            }
-            return *this;
-        }
-
-        // operator [] overload: returns int data of node at position i in linked list
-        reference operator[](size_type i)
-        {
-            return const_cast<reference>(static_cast<const ListInt &>(*this)[i]);
-        }
-
-        // operator [] overload: const overload of above function
-        const_reference operator[](size_type i) const
-        {
-            if (i >= counter)
-            {
-                throw std::out_of_range("ListInt index out of range");
-            }
-            auto current = head;
-            for (size_type j = 0; j < i; ++j)
-            {
-                current = current->next;
-            }
-            return current->data;
-        }
-
-        // push_front: adds value_type to front of the list
-        void push_front(const_reference value)
-        {
-            auto new_node = new Node{value, head};
-            head = new_node;
-            if (tail == nullptr)
-            {
-                tail = head;
-            }
-            ++counter;
-        }
-
-        // push_back: uses tail data member to efficiently add value_type to back of the list
-        void push_back(const_reference value)
-        {
-            auto new_node = new Node{value, nullptr};
-            if (tail == nullptr)
-            {
-                head = new_node;
-                tail = new_node;
-            }
-            else
-            {
-                tail->next = new_node;
-                tail = new_node;
-            }
-            ++counter;
-        }
-
-        // pop_front: returns the value of node at front of the list and then destroys this front node
-        value_type pop_front()
-        {
-            if (head == nullptr)
-            {
-                throw std::runtime_error("pop_front called on empty ListInt");
-            }
-        }
-
-        // Operator overloads
-        ListInt operator+(const ListInt &rhs) const
-        {
-            ListInt result(*this);
-            result += rhs;
-            return result;
-        }
-
-        ListInt operator+(const std::initializer_list<value_type> &rhs) const
-        {
-            ListInt result(*this);
-            result += rhs;
-            return result;
-        }
-
-        friend ListInt operator+(const std::initializer_list<value_type> &lhs, const ListInt &rhs)
-        {
-            ListInt result;
-            result += lhs;
-            result += rhs;
-            return result;
-        }
+        // Overload of operator<< that outputs the list to an ostream
+        friend std::ostream &operator<<(std::ostream &os, const ListInt &rhs);
 
     private:
-        // data encapsulated here
-        void clear()
+        // Definition of the Node struct
+        struct Node
         {
-            while (head != nullptr)
-            {
-                Node *temp = head;
-                head = head->next;
-                delete temp;
-            }
-            tail = nullptr;
-            counter = 0;
-        }
+            value_type data;
+            Node *next;
+
+            // Constructor
+            Node(value_type value) : data(value), next(nullptr) {}
+        };
+
+        Node *head;
+        Node *tail;
+
+        // Static member to keep track of the number of ListInt objects created
         static size_type object_counter;
+
+        // Private member function to create a new node
+        Node *new_node(value_type data);
     };
 
-    // Initialize static member variable
-    ListInt::size_type ListInt::object_counter = 0;
-
-    // Definition of static member function
-    ListInt::size_type ListInt::object_count()
-    {
-        return object_counter;
-    }
-
-    ListInt::size_type ListInt::size() const
-{
-    size_type count = 0;
-    Node *current = head;
-    while (current != nullptr) {
-        ++count;
-        current = current->next;
-    }
-    return count;
-}
-
-bool ListInt::empty() const
-{
-    return head == nullptr;
-}
+    std::ostream &operator<<(std::ostream &os, const ListInt &rhs);
+#endif /* LISTINT_H */
 }
