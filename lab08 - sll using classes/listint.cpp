5ff851861480eb6af47b8229455029af0074e8db
benjamin <benjaminzhiyuan.lee@digipen.edu>
2023-03-03 14:50:45 +0800

    meh

--- lab08 - sll using classes/listint.cpp
+++ lab08 - sll using classes/listint.cpp
@@ -1,8 +1,7 @@
-// File header documentation is required!!!
-
 // Other than listint.hpp, you really don't have to include any other files except when testing your code.
 // When you're ready to submit, don't include either of the following two headers: <forward_list> and <list>
-
+#include <iomanip>
+#include "listint.hpp"
 // Private functions must be declared and defined in an anonymous namespace!!!
 
 // This function definition is provided:
@@ -13,16 +12,21 @@
 //           list - the List to output
 //  Outputs: Returns an ostream object.
 ////////////////////////////////////////////////////////////////////////////////
-std::ostream& operator<<(std::ostream& os, const ListInt& rhs) {
-  // Start at the first node
-  ListInt::Node *pnode = rhs.head;
+namespace hlp2
+{
+  std::ostream &operator<<(std::ostream &os, const ListInt &rhs)
+  {
+    // Start at the first node
+    ListInt::Node *pnode = rhs.head;
 
-  // Until we reach the end of the list
-  while (pnode != 0) {
-    os << std::setw(4) << pnode->data; // print the data in this node
-    pnode = pnode->next;               // move to the next node
+    // Until we reach the end of the list
+    while (pnode != 0)
+    {
+      os << std::setw(4) << pnode->data; // print the data in this node
+      pnode = pnode->next;               // move to the next node
+    }
+
+    os << std::endl; // extra newline for readability
+    return os;
   }
-  
-  os << std::endl; // extra newline for readability
-  return os;
 }
